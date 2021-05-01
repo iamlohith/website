808b04f96acc84d2b910aaa9632b17fb71c5b8de
unknown <lohithe2017@gmail.com>
2021-05-01 16:40:13 +0530

    article ui has better for reading

---
+++ a.js
@@ -0,0 +1,12 @@
+var searchIndex = lunr(function() {
+    this.ref("id");
+    this.field("title", { boost: 10 });
+    this.field("content");
+    for (var key in window.pages) {
+        this.add({
+            "id": key,
+            "title": pages[key].title,
+            "content": pages[key].content
+        });
+    }
+});
